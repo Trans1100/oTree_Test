c748913b4070b394ea6937886f55bfca69972604
Trans1100 <57917275+Trans1100@users.noreply.github.com>
2021-10-10 18:04:21 -0500

    Update __init__.py

--- Quiz_Trust/__init__.py
+++ Quiz_Trust/__init__.py
@@ -72,6 +72,7 @@ def player_correct(player: Player):
 # PAGES
 
 class Question(Page):
+    timeout_seconds = 300
     form_model = "player"
     form_fields = ['answer_1', 'answer_2', 'answer_3', 'answer_4',
                    'answer_5', 'answer_6', 'answer_7', 'answer_8']
